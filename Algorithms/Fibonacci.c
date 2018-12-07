62cf989f331fe3b0b548da6d1f4c2ef7c036fe9e
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2018-12-07 21:49:58 +0900

    Update Fibonacci.c

--- Algorithms/Fibonacci.c
+++ Algorithms/Fibonacci.c
@@ -1,36 +1,17 @@
 #include <stdio.h>
 
+int fibo(int num)
+{
+	if (num == 0 || num == 1)
+		return num;
+	else
+		return fibo(num - 1) + fibo(num - 2);
+}
+
 int main()
 {
 	int n;
-	int result;
-
-	scanf("%d", &n);
-
-	if (2 <= n && n <= 17);
-	
-	else
-		exit(0);
-
-	result = Fib(n);
-	printf("%d", result);
-
+	scanf_s("%d", &n);
+	printf_s("%d\n", fibo(n));
 	return 0;
-}
-
-int Fib(int n)
-{
-	int first = 0;
-	int second = 1;
-	int temp = 0;
-
-	while (n)
-	{
-		first = second;
-		second = temp + first;
-		temp = first;
-		n--;
-	}
-
-	return first;
 }
